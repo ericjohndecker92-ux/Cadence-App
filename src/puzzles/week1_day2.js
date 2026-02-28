a2d41acd5b9fa7d2dae2bf98d781474ef0dc693b
Eric Decker <ericjohndecker@yahoo.com>
2026-02-27 18:23:51 -0600

    Deploy static site to root

--- src/puzzles/week1_day2.js
+++
@@ -1,4 +0,0 @@
-export const puzzleDay2 = {
-  words: ["energetic", "thoughtful", "friendly", "clever", "patient", "curious", "helpful", "strong"],
-  solution: ["energetic", "thoughtful", "friendly", "clever"]
-};
